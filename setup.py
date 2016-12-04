1614dce6db4decf4bb046b38901a7babf405e220
Mehmet Kose <mehmetkose122@gmail.com>
2016-12-04 12:07:11 +0300

    add dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='tornado-uvloop',
-    version='0.1.5',
+    version='0.1.6',
     packages=['tornadouvloop'],
     url='https://github.com/mehmetkose/tornado-uvloop',
     license='DWYW',
@@ -14,14 +14,16 @@ setup(
     keywords=['tornado','asyncio','uvloop'],
     install_requires=[
         'tornado>=4.4.0',
+        'uvloop>=0.6.0',
         'rethinkdb>=2.3.0',
     ],
     classifiers=[
+        'Intended Audience :: Developers',
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: DWYW License',
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Environment :: Web Environment",
         "Topic :: Utilities",
     ],
 )
