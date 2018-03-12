63bc72e7328a21382f17da9fedb9f626e2b6ecc8
Mehmet Kose <mehmetkose122@gmail.com>
2018-03-12 22:10:03 +0300

    update dependencies

--- setup.py
+++ setup.py
@@ -3,19 +3,18 @@ from distutils.core import setup
 
 setup(
     name='tornado-uvloop',
-    version='0.1.6',
+    version='0.2.0',
     packages=['tornadouvloop'],
     url='https://github.com/mehmetkose/tornado-uvloop',
-    license='DWYW',
+    license='MIT',
     author='Mehmet Kose',
-    author_email='mehmet@linux.com',
+    author_email='mehmetkose122@gmail.com',
     description='super simple uvloop class for tornado framework',
     platforms=('linux'),
     keywords=['tornado','asyncio','uvloop'],
     install_requires=[
         'tornado>=4.4.0',
         'uvloop>=0.6.0',
-        'rethinkdb>=2.3.0',
     ],
     classifiers=[
         'Intended Audience :: Developers',
